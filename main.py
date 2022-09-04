3b18b625aee0274c45d5084e22c1a2a2e7d2fa39
Sora <754237913@qq.com>
2022-09-04 23:24:22 +0800

    hot-fix commit1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi world!')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi world!2')
+    print(f'Hi world!3')
 
 
 # Press the green button in the gutter to run the script.
